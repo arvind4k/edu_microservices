a1e5743f1f168f7c8cfabd92b3b023f3b15791c5
arvind4k <to_arvind@yahoo.com>
2017-12-25 17:32:50 -0800

    user signup

--- users/src/main/java/com/talooz/ms/users/entity/User.java
+++ users/src/main/java/com/talooz/ms/users/entity/User.java
@@ -1,5 +1,7 @@
 package com.talooz.ms.users.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,11 +10,16 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "USERS")
-public class User {
+public class User implements Serializable{
 
+	private static final long serialVersionUID = -6985501357877150342L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	private String firstName;
+	private String lastName;
+	private String email;
 	private String username;
 	private String password;
 
@@ -20,8 +27,32 @@ public class User {
 		return id;
 	}
 
-	public void setUserId(Long id) {
+	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getUsername() {
